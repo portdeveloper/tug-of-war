e0d8d805b089c1f3de1b1301cb36f48f1f370b66
portdeveloper <108868128+portdeveloper@users.noreply.github.com>
2025-01-12 08:58:19 +0300

    Update scaffold config

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -1,3 +1,4 @@
+import { defineChain } from "viem";
 import * as chains from "viem/chains";
 
 export type ScaffoldConfig = {
@@ -10,9 +11,27 @@ export type ScaffoldConfig = {
 
 export const DEFAULT_ALCHEMY_API_KEY = "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 
+export const monadDevnet = defineChain({
+  id: Number(process.env.NEXT_PUBLIC_MONAD_CHAIN_ID),
+  name: "Monad Devnet",
+  nativeCurrency: { name: "Monad", symbol: "DMON", decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: [process.env.NEXT_PUBLIC_MONAD_RPC_URL || ""],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Monad Devnet Blockscout",
+      url: process.env.NEXT_PUBLIC_MONAD_BLOCKSCOUT_URL || "",
+    },
+  },
+});
+
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.foundry],
+  targetNetworks: [chains.foundry], // change to monadDevnet when you want to go production like below
+  // targetNetworks: [monadDevnet],
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect if you only target the local network (default is 4000)
